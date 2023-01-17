ff7e1541553e3f63943fd25a3df3d3334b7ffb04
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-01-17 12:04:09 +0100

    Updates & improvements

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    if (!environment.production) {
+      console.warn('hello from your console');
+    }
+  }
+}
