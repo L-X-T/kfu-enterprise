06b36cacb9eeaaa5efb119350e77fc60f9a60a2d
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-01-19 16:16:54 +0100

    [M-F] Reupgrade to Nx15 & NG15

--- libs/passenger/feature-search/src/lib/passenger-feature-search.module.ts
+++ libs/passenger/feature-search/src/lib/passenger-feature-search.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ReactiveComponentModule } from '@ngrx/component';
+import { LetModule, PushModule } from '@ngrx/component';
 import { SearchComponent } from './search.component';
 import { PassengerDomainModule } from '@flight-workspace/passenger/domain';
 
@@ -17,7 +17,7 @@ import { PassengerDomainModule } from '@flight-workspace/passenger/domain';
         component: SearchComponent
       }
     ]),
-    ReactiveComponentModule,
+    LetModule, PushModule,
     PassengerDomainModule
   ],
   declarations: [SearchComponent],
